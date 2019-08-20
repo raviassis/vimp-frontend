4c04d4fabf03653d8c2108656ba9b2e04744e02d
raviassis <ravi.g.assis@gmail.com>
2019-08-20 19:41:23 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files....
+app.use(express.static(__dirname + '/dist/vimp-frontend'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/vimp-frontend/index.html'));
+});
+
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
